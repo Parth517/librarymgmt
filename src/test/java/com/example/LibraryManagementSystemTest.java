22fec5486bd2c2dd527df4e93980d896e076290f
Parth Deshpande <parthjun27@gmail.com>
2024-08-09 23:35:46 +0530

    TEST/Added test for viewAllAvailableBooks functionality

--- src/test/java/com/example/LibraryManagementSystemTest.java
+++ src/test/java/com/example/LibraryManagementSystemTest.java
@@ -3,7 +3,11 @@ package com.example;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 import static org.junit.Assert.fail;
+import static org.junit.jupiter.api.Assertions.assertEquals;
+import static org.junit.jupiter.api.Assertions.assertTrue;
+import static org.junit.jupiter.api.Assertions.fail;
 
+import java.util.List;
 
 import org.junit.Before;
 import org.junit.Test;
@@ -182,20 +186,39 @@ public class LibraryManagementSystemTest {
             fail("Exception should not be raised here");
         }
     }
-   @Test
-public void shouldReturnBookAndIncrementCount() {
-    try {
-        book1 = new Books("Effective Coding", "Abc", "1234567891234", 2018);
-        libraryManagementSystem.addBook(book1);
-        libraryManagementSystem.borrowBook("1234567891234");
-        assertEquals(0, libraryManagementSystem.countCopiesByIsbn("1234567891234"));
 
-        // Return the book
-        libraryManagementSystem.returnBook(book1);
-        assertEquals(1, libraryManagementSystem.countCopiesByIsbn("1234567891234"));
+    @Test
+    public void shouldReturnBookAndIncrementCount() {
+        try {
+            book1 = new Books("Effective Coding", "Abc", "1234567891234", 2018);
+            libraryManagementSystem.addBook(book1);
+            libraryManagementSystem.borrowBook("1234567891234");
+            assertEquals(0, libraryManagementSystem.countCopiesByIsbn("1234567891234"));
 
-    } catch (invalidBookDetailsException | bookNotAvailableException e) {
-        fail("Exception should not be raised here");
+            // Return the book
+            libraryManagementSystem.returnBook(book1);
+            assertEquals(1, libraryManagementSystem.countCopiesByIsbn("1234567891234"));
+
+        } catch (invalidBookDetailsException | bookNotAvailableException e) {
+            fail("Exception should not be raised here");
+        }
     }
-}
+
+    @Test
+    public void shouldShowAllAvailableBook() {
+        try {
+            book1 = new Books("Effective Coding", "Abc", "1234567891234", 2018);
+            book2 = new Books("Animal Farm", "George Orwell", "0123456789123", 1956);
+            libraryManagementSystem.addBook(book1);
+            libraryManagementSystem.addBook(book2);
+
+            List<Books> AvailableBooks = libraryManagementSystem.viewAllAvailableBooks();
+            assertTrue(availableBooks.contains(book1));
+            assertTrue(availableBooks.contains(book2));
+            assertEquals(2, availableBooks.size());
+
+        } catch (invalidBookDetailsException e) {
+            fail("Exception should not be thrown for valid books");
+        }
+    }
 }

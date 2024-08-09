9190c355772402712b6560523a03d7fc03e39317
Parth Deshpande <parthjun27@gmail.com>
2024-08-09 11:57:04 +0530

    FEAT/Initializing LibraryManagementSystem class

--- src/test/java/com/example/LibraryManagementSystemTest.java
+++ src/test/java/com/example/LibraryManagementSystemTest.java
@@ -1,5 +1,17 @@
 package com.example;
 
+import static org.junit.Assert.assertEquals;
+
+import org.junit.Test;
+import org.junit.jupiter.api.AfterAll;
+import org.junit.jupiter.api.BeforeEach;
+
 public class LibraryManagementSystemTest {
     private LibraryManagementSystem libraryManagementSystem;
+
+    @BeforeEach
+    public void setup() {
+        libraryManagementSystem = new LibraryManagementSystem();
+    }
+    
 }

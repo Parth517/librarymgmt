46331471be4170ded044b2a2f9af5200df6c1862
Parth Deshpande <parthjun27@gmail.com>
2024-08-09 16:46:48 +0530

    FEAT/Implemented countCopiesByIsbn to count books

--- src/main/java/com/example/LibraryManagementSystem.java
+++ src/main/java/com/example/LibraryManagementSystem.java
@@ -7,13 +7,35 @@ public class LibraryManagementSystem {
 
 
     private static List<Books> BooksInLibrary = new ArrayList<>();
+    private static List<String[]> countNumberOfBooks = new ArrayList<>();
 
     public void addBook(Books book) {
         BooksInLibrary.add(book);
+        boolean found=false;
+        //count number of books if book is already in countNumberOfBooks
+        for (String[] entry : countNumberOfBooks) {
+            if (entry[0].equals(book.getIsbn())) {
+                entry[1] = String.valueOf(Integer.parseInt(entry[1]) + 1);
+                found = true;
+                break;
+            }
+        }
+        if(!found){
+            countNumberOfBooks.add(new String[]{book.getIsbn(),"1"});
+        }
         System.out.println("Book added Successfully " + book.getTitle());
     }
 
     public static List<Books> getBooksInLibrary() {
         return BooksInLibrary;
+    }
+
+    public int countCopiesByIsbn(String isbn){
+        for(String[] entry:countNumberOfBooks){
+            if (entry[0].equals(isbn)) {
+                return Integer.parseInt(entry[1]);
+            }
+        }
+        return 0;
     }
 }

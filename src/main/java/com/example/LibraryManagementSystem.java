fce16f5026e7a350513fa082a2b217889b6a0ab9
Parth Deshpande <parthjun27@gmail.com>
2024-08-09 12:23:19 +0530

    FEAT/Implemented the addBook functionality to add books

--- src/main/java/com/example/LibraryManagementSystem.java
+++ src/main/java/com/example/LibraryManagementSystem.java
@@ -1,4 +1,62 @@
 package com.example;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class LibraryManagementSystem {
+
+    private String title;
+    private String author;
+    private String isbn;
+    private int publicationYear;
+
+    private static List<LibraryManagementSystem> BooksInLibrary = new ArrayList<>();
+
+    public LibraryManagementSystem(String title, String author, String isbn, int publicationYear) {
+        this.title = title;
+        this.author = author;
+        this.isbn = isbn;
+        this.publicationYear = publicationYear;
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public String getIsbn() {
+        return isbn;
+    }
+
+    public void setIsbn(String isbn) {
+        this.isbn = isbn;
+    }
+
+    public int getPublicationYear() {
+        return publicationYear;
+    }
+
+    public void setPublicationYear(int publicationYear) {
+        this.publicationYear = publicationYear;
+    }
+
+    public void addBook(LibraryManagementSystem libraryManagementSystem) {
+        BooksInLibrary.add(libraryManagementSystem);
+        System.out.println("Book added Successfully " + libraryManagementSystem.getTitle());
+    }
+
+    public static List<LibraryManagementSystem> getBooksInLibrary(){
+        return BooksInLibrary;
+    }
 }
--- src/test/java/com/example/LibraryManagementSystemTest.java
+++ src/test/java/com/example/LibraryManagementSystemTest.java
@@ -3,6 +3,7 @@ package com.example;
 import static org.junit.Assert.assertEquals;
 import static org.junit.Assert.assertTrue;
 
+import org.junit.Before;
 import org.junit.Test;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeEach;
@@ -10,13 +11,13 @@ import org.junit.jupiter.api.BeforeEach;
 public class LibraryManagementSystemTest {
     private LibraryManagementSystem libraryManagementSystem;
 
-    @BeforeEach
+    @Before
     public void setup() {
         libraryManagementSystem = new LibraryManagementSystem("Effective Coding","Abc","1234567891234",2018);
     }
     @Test
     public void shouldAddBook(){
-        libraryManagementSystem.addBook(book1);
-        assertTrue(libraryManagementSystem.getBooksInLibrary().contains(book1));
+        libraryManagementSystem.addBook(libraryManagementSystem);
+        assertTrue(libraryManagementSystem.getBooksInLibrary().contains(libraryManagementSystem));
     }
 }
